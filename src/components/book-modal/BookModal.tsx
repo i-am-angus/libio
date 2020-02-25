537703dcddfaf75e40832a0eb0ca41aca949e70d
Angus Burroughs <angus.burroughs@payd.co>
2020-02-25 23:41:28 +0200

    checkout button dummied with alert

--- src/components/book-modal/BookModal.tsx
+++ src/components/book-modal/BookModal.tsx
@@ -27,6 +27,17 @@ const useStyles = makeStyles((theme: Theme) =>
   })
 );
 
+function checkoutButtonClick(e :any, book: Book) {
+  alert(`Checkout button clicked for book: ${book.title}`)
+}
+
+function statusBox(book: Book) {
+
+  return book.status ? (<span>Currently checked out by {book.holderEmail}</span>)
+                     : (<button onClick={(e) => {checkoutButtonClick(e, book)}}> Check out </button>)
+
+}
+
 export default function BookModal({open, close, book}: {open: any, close: any, book: Book}) {
   const classes = useStyles();
   // getModalStyle is not a pure function, we roll the style only on the first render
@@ -43,9 +54,17 @@ export default function BookModal({open, close, book}: {open: any, close: any, b
       >
         <div style={modalStyle} className={classes.paper}>
           <h2 id="simple-modal-title">{book.title}</h2>
-          <p id="simple-modal-description">
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </p>
+          
+          <div>Image goes here</div>
+          <br />
+          <span>Title: {book.title}</span>
+          <br></br>
+          <span>Author: {book.authors}</span>
+          <br></br>
+          <span>Tags: {book.tags}</span>
+          <br></br>
+          {statusBox(book)}
+
         </div>
       </Modal>
     );
